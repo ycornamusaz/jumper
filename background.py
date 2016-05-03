5e33a1018e6975edb2e301651ad95bfe5bc4db7b
Yoël Cornamusaz <yoel.cornamusaz@camptocamp.com>
2016-05-03 15:42:44 +0200

    Add "game over" screen

--- background.py
+++ background.py
@@ -10,6 +10,7 @@ class Background() :
         self.bg2_base = pygame.image.load("PNG/Background/bg_layer2.png")
         self.bg3_base = pygame.image.load("PNG/Background/bg_layer3.png")
         self.bg4_base = pygame.image.load("PNG/Background/bg_layer4.png")
+        self.bg_game_over = pygame.image.load("PNG/Background/red.png")
 
         ## Creat background surface
         self.bg = pygame.Surface([Config.width, Config.height])
@@ -19,6 +20,22 @@ class Background() :
         self.bg2 = pygame.transform.scale(self.bg2_base, (Config.width, Config.height))
         self.bg3 = pygame.transform.scale(self.bg3_base, (Config.width, Config.height))
         self.bg4 = pygame.transform.scale(self.bg4_base, (Config.width, Config.height))
+        self.bg_game_over = pygame.transform.scale(self.bg_game_over, (Config.width, Config.height))
+
+        ## Fuse background onto background surface
+        self.reset()
+
+    def update(self, screen) :
+
+        ## Print background on screen
+        screen.blit(self.bg, [0, 0])
+
+    def game_over(self) :
+        
+        ## Add red filter to background
+        self.bg.blit(self.bg_game_over, [0, 0])
+
+    def reset(self) :
         
         ## Fuse background onto background surface
         self.bg.blit(self.bg1, [0, 0])
@@ -26,7 +43,3 @@ class Background() :
         self.bg.blit(self.bg3, [0, 0])
         self.bg.blit(self.bg4, [0, 0])
 
-    def update(self, screen) :
-
-        ## Print background on screen
-        screen.blit(self.bg, [0, 0])
--- jumper.py
+++ jumper.py
@@ -24,6 +24,7 @@ class Game :
         clock = pygame.time.Clock()
         ## Menu loop stat
         done_menu = False
+        start_game = False
         ## Set background
         background = Background()
         ## Create menu sprite group
@@ -68,8 +69,8 @@ class Game :
                         buton_pointer_list = pygame.sprite.spritecollide(pointer, buton_list, True)
                         for buton in buton_pointer_list :
                             if buton.text is "Play" :
-                                #game_start = True
-                                Game.game()
+                                start_game = True
+
                             elif buton.text is "Options" :
                                 buton.text = "This is currently not implemented"
                                 buton.update("This is currently not implemented", Color.WHITE)
@@ -79,6 +80,12 @@ class Game :
         
             ########## LOGIC CODE ZONE ##########
             
+            while start_game == True :
+                Game.game()
+                start_game = Game.game_over()
+            if start_game == None :
+                break
+
             ## Update mouse pos
             pos = pygame.mouse.get_pos()
             pointer.rect.x = pos[0]
@@ -138,19 +145,12 @@ class Game :
         
         ## Set game loop stat
         done_game = False
+
         ## Create game sprite groups
         player_list = pygame.sprite.Group()
         ground_list = pygame.sprite.Group()
         movable_list = pygame.sprite.Group()
         all_game_sprites_list = pygame.sprite.Group()
-#        ## Create player and set player direction
-#        player = Player("male")
-#        direction = "stand"
-#        player_list.add(player)
-#        ## Add player to gamesprite's group
-#        movable_list.add(player)
-#        ## Add player to movable group
-#        all_game_sprites_list.add(player)
 
         ## Gen players
         player = conf.gen_players(player_list, [movable_list, all_game_sprites_list])
@@ -203,40 +203,6 @@ class Game :
                             ## Set player speed to 0
                             player[i].speed = 0
                     
-#                    ## If any key is pressed
-#                    elif event.type == pygame.KEYDOWN :
-#                        
-#                        ## If right key is pressed
-#                        if event.key == pygame.K_RIGHT :
-#                            ## Move player to right
-#                            player.speed = player.speed_base
-#                            ## Change animation
-#                            direction = "right"
-#    
-#                        ## If left key is pressed
-#                        elif event.key == pygame.K_LEFT :
-#                            ## Move player to left
-#                            player.speed = -(player.speed_base)
-#                            ## Change animation
-#                            direction = "left"
-#    
-#                        ## If key up is pressed
-#                        elif event.key == pygame.K_UP :
-#                            ## Check if player in on the ground before jump
-#                            if player.on_ground == True :
-#                                player.last_y = player.rect.y
-#                                player.in_jump = True
-#                    
-#                    ## If key is release
-#                    elif event.type == pygame.KEYUP : 
-#                        
-#                        ## If key right or key left is release 
-#                        if (event.key == pygame.K_RIGHT) or (event.key == pygame.K_LEFT) :
-#                            ## Set player speed to 0
-#                            player.speed = 0
-#                            direction = "stand"
-        
-        
             ########## LOGIC CODE ZONE ##########
             
             for i in player :
@@ -279,4 +245,118 @@ class Game :
             ## Set game tick (per second)
             clock.tick(120)
 
-Game.menu()
+    def game_over() :
+
+        ## Init pygame
+        pygame.init()
+        ## Init screen
+        screen = pygame.display.set_mode([Config.width, Config.height])
+        ## Init windows title
+        pygame.display.set_caption("JUMPER !!!")
+        ## Hide mouse cursor
+        pygame.mouse.set_visible(False)
+        ## Init clock
+        clock = pygame.time.Clock()
+        ## Menu loop stat
+        done_game_over = False
+        ## Set background
+        background_red = Background()
+        background_red.game_over()
+        ## Create menu sprite group
+        buton_list = pygame.sprite.Group()
+        all_game_over_sprites_list = pygame.sprite.Group()
+        ## Create pointer sprite
+        pointer = Pointer()
+        ## Add pointer to menu sprite's group
+        all_game_over_sprites_list.add(pointer)
+        ## Create butons
+        buton1 = Buton("Restart", Color.WHITE)
+        buton2 = Buton("Quit", Color.WHITE)
+        ## Set buton1 pos
+        buton1.rect.x = (Config.width/2 - buton1.width/2)
+        buton1.rect.y = (Config.height/2 - buton1.height/2 - 100)
+        ## Set buton2 pos
+        buton2.rect.x = (Config.width/2 - buton2.width/2)
+        buton2.rect.y = (Config.height/2 - buton2.height/2 + 100)
+        ## Add butons to buton list
+        buton_list.add(buton1)
+        buton_list.add(buton2)
+        ## Add butons to menu sprite's group
+        all_game_over_sprites_list.add(buton1)
+        all_game_over_sprites_list.add(buton2)
+    
+        ## Start loop
+        while not done_game_over :
+            
+            ########## EVENT ZONE ##########
+        
+            ## For every events, filter event and refresh screen
+            for event in pygame.event.get() :
+        
+                ## Filter events
+                ## If the cross is pressed, quit game
+                if event.type == pygame.QUIT :
+                    done_game_over = True
+                ## If any mouse buton is pressed 
+                if event.type == pygame.MOUSEBUTTONDOWN :
+                    ## If pointer is on buton1, start game
+                    if event.button == 1 :
+                        buton_pointer_list = pygame.sprite.spritecollide(pointer, buton_list, True)
+                        for buton in buton_pointer_list :
+                            if buton.text is "Restart" :
+                                return True
+                                done_game_over = True
+                            elif buton.text is "Quit" :
+                                done_game_over = True
+                                return None
+
+                            all_game_over_sprites_list.add(buton)
+                            buton_list.add(buton)
+        
+            ########## LOGIC CODE ZONE ##########
+            
+            ## Update mouse pos
+            pos = pygame.mouse.get_pos()
+            pointer.rect.x = pos[0]
+            pointer.rect.y = pos[1]
+            
+            ## Detect rect colision between pointer and buton group
+            buton_pointer_list = pygame.sprite.spritecollide(pointer, buton_list, True)
+            
+            ## If a rect colision is detected
+            if buton_pointer_list != [] : 
+                ## For each buton who are in colision with pointer
+                for buton in buton_pointer_list :
+                    ## Update text Color to red
+                    if buton.color != Color.RED :
+                        buton.update(buton.text , Color.RED)
+                    ## Re-add buton to sprite list
+                    buton_list.add(buton)
+                    all_game_over_sprites_list.add(buton)
+            ## If not
+            else :
+                ## For all butons 
+                for buton in buton_list :
+                    ## Update text Color to white
+                    if buton.color != Color.WHITE :
+                        buton.update(buton.text, Color.WHITE)
+            
+            ########## CLEAR SCREEN ZONE ##########
+        
+            ## Set the background
+            background_red.update(screen)
+        
+            ########## DRAWING CODE ZONE ##########
+            
+            ## Draw all sprites to the screen
+            all_game_over_sprites_list.draw(screen)
+        
+            ########## REFRESH SCREEN ZONE ##########
+        
+            ## Refresh screen
+            pygame.display.flip()
+        
+            ## Set game ticks (per second)
+            clock.tick(60)
+
+Game.menu() 
